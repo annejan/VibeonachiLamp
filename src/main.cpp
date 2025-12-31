189e9dcd517ef3b468eb0cf8c34b84e42e11cc90
Anne Jan Brouwer <brouwer@annejan.com>
2025-12-31 18:47:26 +0100

    ESP8266 support

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
-#include <Arduino.h>
-#include <FastLED.h>
+    #include <Arduino.h>
+    #include <FastLED.h>
 
-#define LED_PIN     6
-#define NUM_LEDS    47
-#define BRIGHTNESS  255
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
+    //#define LED_PIN     6
+    #define LED_PIN     D4
+    #define NUM_LEDS    47
+    #define BRIGHTNESS  255
+    #define LED_TYPE    WS2812B
+    #define COLOR_ORDER RGB
 
-CRGB leds[NUM_LEDS];
+    CRGB leds[NUM_LEDS];
 
-void setup() {
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
-    FastLED.setBrightness(BRIGHTNESS);
-}
-
-void loop() {
-    static uint8_t hue = INT8_MAX;
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(hue + i * PI, 255, 255);
+    void setup() {
+        FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+        FastLED.setBrightness(BRIGHTNESS);
     }
 
-    FastLED.show();
-    hue--;
-    delay(1);
-}
+    void loop() {
+        static uint8_t hue = 0;
+
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CHSV(hue + i * 4, 255, 255);
+        }
+
+        FastLED.show();
+        hue--;
+        delay(1);
+    }
